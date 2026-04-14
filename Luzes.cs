85d3e44ae699814453e166ab7aee2a8bf83d9a8a
anaclaralopeslouzada <ana.louzada@unifesp.br>
2026-04-14 16:38:53 -0300

    Create Luzes.cs

---
+++ Luzes.cs
@@ -0,0 +1,7 @@
+namespace HomeTheater
+{
+    public class Luzes {
+        public void Escurecer() => Console.WriteLine("Luzes: Escurecendo o ambiente...");
+        public void Acender() => Console.WriteLine("Luzes: Luzes acesas.");
+    }
+}
